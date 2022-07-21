5a2894935563148b628ce78101b465df9b661681
IvanGerman <ivgvi@mail.ru>
2022-07-21 19:42:33 +0700

    feat: first try of simple books route guarding vs unauthorized users

--- src/pages/startPage/StartPage.js
+++ src/pages/startPage/StartPage.js
@@ -1,6 +1,8 @@
+import { isUserAuth } from '../../state/data';
 import './StartPage.scss';
 
 const StartPage = {
+
   render: async () => {
     const view = `
       <div class="start-page-div">
@@ -12,6 +14,24 @@ const StartPage = {
   
   after_render: () => {
     console.log('startPage rendered, now we can start some logic part');
+    const booksLink = document.querySelector('#booksLink');
+    //to change, it adds more and more eventlisteners
+    booksLink.addEventListener('click', (event) => {
+      event.preventDefault();
+      console.log('booksLink');
+      console.log('isUserAuth--',isUserAuth, typeof(isUserAuth));
+      if ( isUserAuth === true ) {
+       // document.URL is the current url
+      var url_ob = new URL(document.URL);
+      url_ob.hash = 'books';
+
+      // new url
+      var new_url = url_ob.href;
+
+      // change the current url
+      document.location.href = new_url;
+      }
+    })
   },
 };
 
