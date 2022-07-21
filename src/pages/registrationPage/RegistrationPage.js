40794730dd333b91a4c416af33bb460e07e23fcc
IvanGerman <ivgvi@mail.ru>
2022-07-21 18:03:09 +0700

    feat: add registration fields and send their data via POST request to server and save it in MongoDB

--- src/pages/registrationPage/RegistrationPage.js
+++ src/pages/registrationPage/RegistrationPage.js
@@ -5,13 +5,43 @@ const RegistrationPage = {
     const view = `
       <div class="registration-page-div">
       RegistrationPage
+      <br><br>
+  <label for="regEmail">Email:</label>
+  <input type="text" id="regEmail" name="regEmail"><br><br>
+  <label for="regPassword">Password:</label>
+  <input type="text" id="regPassword" name="regPassword"><br><br>
+  <input type="button" value="Submit" id="regBtn">
+
       </div>
     `;
     return view;
   },
   
   after_render: () => {
+
     console.log('RegistrationPage rendered, now we can start some logic part');
+    
+    const regBtn = document.querySelector('#regBtn');
+    const regEmail = document.querySelector('#regEmail');
+    const regPassword = document.querySelector('#regPassword');
+
+    function myFunc() {
+      console.log('post request sent', regEmail.value, regPassword.value);
+      fetch('http://localhost:5000/api/auth/register', {
+        method: 'POST', 
+        mode: 'cors', 
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+          {'email': regEmail.value,
+            'password': regPassword.value
+          }) // body data type must match "Content-Type" header
+      });
+    };
+
+    regBtn.addEventListener('click', myFunc);
+
   },
 };
 
